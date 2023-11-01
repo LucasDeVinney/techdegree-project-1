697aa736859ff411e4aa89297f9851165c109e70
Lucas DeVinney <lucasdevinney12@gmail.com>
2023-11-01 14:59:28 -0400

    Added background color change and media type

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
+// NOTE: Utilized Stack Overflow (https://stackoverflow.com/questions/5195303/set-css-property-in-javascript)
+// to learn about how to access css styles of an element.
+// Used only at line 112
+
 // Creates array of quote objects.
 const quotes = [
-  { // quote object includes quote, source, and citation (if present)
+  { // quote object includes quote, source, citation and type of content the quote was taken from (typeOfContent)
     quote: "Three can keep a secret, if two of them are dead.",
     source: "Benjamin Franklin",
     year: 1735,
@@ -21,13 +25,15 @@ const quotes = [
   {
     quote: "To err is human; to forgive, divine.",
     source: "Alexander Pope",
-    citation: "An Essay on Criticism to the US Institutes of Medicine's report on patient safety: To Err is Human"
+    citation: "An Essay on Criticism to the US Institutes of Medicine's report on patient safety: To Err is Human",
+    typeOfContent: 'essay'
   },
   {
     quote: "Go ahead, make my day.",
     source: "Harry Callahan (character)",
     citation: "Sudden Impact",
-    year: 1983
+    year: 1983,
+    typeOfContent: 'movie'
   }
 ];
 
@@ -45,10 +51,28 @@ function getRandomQuote(arr) {
   return arr[randomNum]; // Returns random quote object
 }
 
-// console.log(getRandomQuote(quotes)); successful test of getRandomQuote
+/**
+ * Generates a random rgb color value
+ * 
+ * @returns randomColor
+ */
+function getRandomColor() {
+  let randomColor =  `rgb(`;
+  for (let i = 0; i < 3; i++) {
+    randomColor += Math.floor(Math.random() * 255);
+    // If statement that make sure a comma isnt added after the last element.
+    if (i < 2) {
+      randomColor += `, `;
+    }
+  }
+  randomColor += `)`;
+  return randomColor;
+}
+
+console.log(getRandomColor());
 
 /**
- * Adds an html snippet to the linked html document.
+ * Adds an html snippet to the linked html document & changes background color.
  */
 
 function printQuote() {
@@ -57,18 +81,23 @@ function printQuote() {
 
   // 2. Create a variable that initiates the HTML string
   let html = `<p class="quote">${quoteObject.quote}</p>
-  <p class="source">${quoteObject.source}
-  `;
+  <p class="source">${quoteObject.source}`;
 
   // 3. If statement to check if the citation property exists
   if (quoteObject.citation) {
-    html += `<span class="citation">${quoteObject.citation};
+    html += `<span class="citation">${quoteObject.citation}</span>;
     `;
   }
 
   // 4. If statement to check if the year property exists
   if (quoteObject.year) {
-    html += `<span class="year">${quoteObject.year};
+    html += `<span class="year">${quoteObject.year}</span>;
+    `;
+  }
+
+  // If statement to check if the typeOfContent property exists
+  if (quoteObject.typeOfContent) {
+    html += `<span class="typeOfContent">${quoteObject.typeOfContent}</span>;
     `;
   }
 
@@ -77,6 +106,10 @@ function printQuote() {
 
   // 6. Adds complete string to the HTML document
   document.getElementById('quote-box').innerHTML = html; 
+
+  // Change background color
+  let element = document.querySelector('body');
+  element.style.backgroundColor = getRandomColor();
 }
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
